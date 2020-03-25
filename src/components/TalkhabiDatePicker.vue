2505e99f21cab56065878aaf62411bfce20b3a0c
Farshad Fakhar <farshadfakhar@Farshads-MacBook-Pro-2.local>
2020-03-25 15:49:00 +0430

    add talkhabiDatePicker

---
+++ src/components/TalkhabiDatePicker.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="inputContainerClass">
+    <label :for="id">{{ label }}</label>
+    <date-picker
+      :id="id"
+      :name="name"
+      :ref="name"
+      :type="type"
+      :class="inputClass"
+      :placeholder="placeHolder"
+      v-model="content"
+      @input="handleInput(content)"
+    />
+  </div>
+</template>
+<script>
+import VuePersianDatetimePicker from "vue-persian-datetime-picker";
+import InputCommons from "./InputCommons.vue";
+export default {
+  extends: InputCommons,
+  components: {
+    datePicker: VuePersianDatetimePicker
+  }
+};
+</script>
