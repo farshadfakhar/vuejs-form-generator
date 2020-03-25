ea6d6809ced452761db605e91528e7a91bd26dc3
Farshad Fakhar <farshadfakhar@Farshads-MacBook-Pro-2.local>
2020-03-25 15:49:13 +0430

    dry

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div :class="inputContainerClass">
     <label :for="id">{{ label }}</label>
     <input
       :id="id"
       :name="name"
       :ref="name"
       :type="type"
-      :class="classes"
+      :class="inputClass"
       :placeholder="placeHolder"
       v-model="content"
       @input="handleInput(content)"
@@ -14,50 +14,8 @@
   </div>
 </template>
 <script>
+import InputCommons from "./InputCommons.vue";
 export default {
-  props: {
-    label: {
-      type: String,
-      default: ""
-    },
-    id: {
-      default: Date.now()
-    },
-    type: {
-      type: String,
-      default: "text"
-    },
-    classes: {
-      type: Object
-    },
-    placeHolder: {
-      type: String
-    },
-    rules: {
-      type: String
-    },
-    name: {
-      type: String
-    },
-    value: {}
-  },
-  watch: {
-    value: {
-      handler(val) {
-        this.content = val;
-      },
-      deep: true
-    }
-  },
-  data() {
-    return {
-      content: this.value
-    };
-  },
-  methods: {
-    handleInput() {
-      this.$emit("input", this.content);
-    }
-  }
+  extends: InputCommons
 };
 </script>
